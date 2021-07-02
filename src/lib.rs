a1d290caebd950cac616cee73bf96f33bca753d7
msi <mbsd@m0x.ru>
2021-07-01 22:44:03 -0300

    feat: parse options header

--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,95 @@
-/// Tipos de identificação móvel.
-/// 
-///
-#[derive(Debug, PartialEq, Eq)]
-pub enum MobileIDType {
-    Off,
-    Esn,
-    Imei,
-    Eid,
-    Imsi,
-    Gsm,
-    Gprs,
-    UserDefined,
-    PhoneNumber,
-    LmuIp,
+//
+// Copyright (c) 2021 Murilo Ijanc' <mbsd@m0x.ru>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+//
+
+const OPTIONS_BYTE: u8 = 0x83;
+
+#[derive(Debug)]
+pub struct OptionsHeader {
+    pub mobile_id: String,
+    pub mobile_type: u8,
+    next_part: usize,
 }
 
-impl MobileIDType {
-    pub fn id(&self) -> i32 { 
-        match *self {
-            Self::Esn => 1,
-            Self::Imei | Self::Eid => 2,
-            Self::Imsi | Self::Gsm| Self::Gprs => 3,
-            Self::UserDefined => 4,
-            Self::PhoneNumber => 5,
-            Self::LmuIp => 6,
-            _ => 0,
+impl OptionsHeader {
+    pub fn new(mobile_id: &str, mobile_type: u8, next_part: usize) -> Self {
+        Self {
+            mobile_id: mobile_id.to_string(),
+            mobile_type,
+            next_part,
         }
     }
 
-    pub fn to_bytes(&self) -> [u8; 4] {
-        let id = self.id();
-        id.to_be_bytes()
+    fn parse_options(data: &[u8]) -> Option<OptionsHeader> {
+        let mut i: usize = 0;
+        if data[i] == OPTIONS_BYTE {
+            i += 1;
+            let mobile_id_field_length: u8 = data[i];
+            i += 1;
+            let start = i;
+            let mut mobile_id = String::from("");
+            while i < start + mobile_id_field_length as usize {
+                mobile_id.push_str(&format!("{0:2x}", data[i]));
+                i += 1;
+            }
+            i += 1;
+            let _mobile_type_length = data[i];
+            i += 1;
+            let mobile_type = data[i];
+            i += 1;
+            Some(OptionsHeader::new(&mobile_id, mobile_type, i))
+        } else {
+            None
+        }
     }
 
-    pub fn gsm(&self) -> String {
-        let id = self.id();
-        id.to_string()
+    pub fn next_part(&self) -> usize {
+        self.next_part
     }
 }
 
-impl Default for MobileIDType {
-    fn default() -> Self {
-        MobileIDType::Off
+pub fn parse(data: &[u8]) -> OptionsHeader {
+    match OptionsHeader::parse_options(data) {
+        Some(option) => option,
+        None => panic!("nao possui option"),
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::MobileIDType;
-
-    #[test]
-    fn mobile_id_type_default() {
-        assert_eq!(MobileIDType::default(), MobileIDType::Off)
-    }
-
-    #[test]
-    fn mobile_id_type_id() {
-        let esn = MobileIDType::Esn;
-        assert_eq!(esn.id(), 1)
-    }
-
-    #[test]
-    fn mobile_id_type_to_bytes() {
-        let esn = MobileIDType::Esn;
-        assert_eq!(esn.to_bytes(), [0x00, 0x00, 0x00, 0x01]);
-    }
-
-    #[test]
-    fn mobile_id_type_gsm() {
-        let esn = MobileIDType::Esn;
-        assert_eq!(esn.gsm(), String::from("1"));
-    }
+    use super::parse;
 
     #[test]
     fn test_parse_options() {
-        let _data: [u8; 117] = [
-            0x83, 0x05, 0x46, 0x34, 0x66, 0x32, 0x35, 0x01,
-            0x01, 0x01, 0x02, 0x3a, 0x86, 0x5f, 0xf1, 0x3a,
-            0x54, 0x5f, 0xf1, 0x3a, 0x57, 0xf1, 0xe2, 0x85,
-            0x78, 0xe4, 0x22, 0xd6, 0x40, 0x00, 0x01, 0x36,
-            0xf8, 0x00, 0x00, 0x00, 0x0b, 0x00, 0x00, 0x06,
-            0x20, 0x00, 0x00, 0xff, 0x8d, 0x02, 0x1e, 0x1e,
-            0x00, 0x7b, 0x21, 0x10, 0x00, 0x00, 0x00, 0x31,
-            0xe0, 0x00, 0x00, 0x10, 0x1a, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x22, 0x2a, 0x32, 0x00, 0x00, 0x03,
-            0xf1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x01, 0xc8, 0x2d, 0x3f, 0x01, 0xc8, 0x2d,
-            0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x40, 0x01, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00
+        let data: [u8; 117] = [
+            0x83, 0x05, 0x46, 0x34, 0x66, 0x32, 0x35, 0x01, 0x01, 0x01, 0x02,
+            0x3a, 0x86, 0x5f, 0xf1, 0x3a, 0x54, 0x5f, 0xf1, 0x3a, 0x57, 0xf1,
+            0xe2, 0x85, 0x78, 0xe4, 0x22, 0xd6, 0x40, 0x00, 0x01, 0x36, 0xf8,
+            0x00, 0x00, 0x00, 0x0b, 0x00, 0x00, 0x06, 0x20, 0x00, 0x00, 0xff,
+            0x8d, 0x02, 0x1e, 0x1e, 0x00, 0x7b, 0x21, 0x10, 0x00, 0x00, 0x00,
+            0x31, 0xe0, 0x00, 0x00, 0x10, 0x1a, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x22, 0x2a, 0x32, 0x00, 0x00, 0x03, 0xf1, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x01, 0xc8, 0x2d, 0x3f, 0x01, 0xc8, 0x2d,
+            0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x01, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         ];
 
-        assert_eq!(2 + 2, 4);
+        let option = parse(&data);
+        assert_eq!(option.mobile_type, 1);
+        assert_eq!(option.mobile_id, String::from("4634663235"));
+        assert_eq!(option.next_part(), 10);
     }
 }
